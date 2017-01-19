66cd01f00fd004f4a3a7f9d0aad9e4d449994578
hackjutsu <wenzhe89929@ucla.edu>
2017-01-18 20:34:17 -0800

    Relaunch the app when the icon is clicked on Mac
    On macOS, if an app is not fully closed, it is expected to open again
    when the icon is clicked.

--- main.js
+++ main.js
@@ -15,17 +15,40 @@ initGlobalLogger()
 
 let mainWindow = null
 
-app.on('ready', () => {
+function createWindow () {
   mainWindow = new BrowserWindow({
-      width: 1000,
-      height: 700,
-      minWidth: 1000,
-      minHeight: 700,
-      titleBarStyle: 'hidden'
+    width: 1000,
+    height: 700,
+    minWidth: 1000,
+    minHeight: 700,
+    titleBarStyle: 'hidden'
   })
   mainWindow.loadURL(`file://${__dirname}/index.html`)
+  setUpApplicationMenu()
   // mainWindow.webContents.openDevTools()
+}
 
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function() {
+  logger.info('The app window is closed')
+  if (process.platform !== 'darwin') app.quit()
+  mainWindow = null
+})
+
+app.on('before-quit', function() {
+
+})
+
+app.on('activate', () => {
+  // On macOS, if an app is not fully closed, it is expected to open again
+  // when the icon is clicked.
+  if (process.platform === 'darwin' && mainWindow === null) {
+    createWindow()
+  }
+})
+
+function setUpApplicationMenu () {
   // Create the Application's main menu
   let template = [{
     label: "Application",
@@ -46,24 +69,7 @@ app.on('ready', () => {
     ]}
   ]
   Menu.setApplicationMenu(Menu.buildFromTemplate(template))
-})
-
-app.on('window-all-closed', function() {
-  logger.info('The app window is closed')
-  // mainWindow.webContents.session.clearStorageData([
-  //     'appcache',
-  //     'cookies',
-  //     'indexdb',
-  //     'shadercache',
-  //     'websql',
-  //     'serviceworkers',
-  //   ], () => {})
-  if (process.platform !== 'darwin') app.quit()
-})
-
-app.on('before-quit', function() {
-
-})
+}
 
 function initGlobalLogger () {
   logger.level = 'debug'
