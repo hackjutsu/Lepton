0b0c7f44a0c1ee499b57bf4c00c630e1c5ffa06f
hackjutsu <cosmo.lepton@gmail.com>
2018-03-21 23:23:23 -0700

    wired the radar chart with gist stats

--- app/containers/Dashboard/index.js
+++ app/containers/Dashboard/index.js
@@ -6,23 +6,51 @@ import { connect } from 'react-redux'
 import { Modal, Image } from 'react-bootstrap'
 import { Radar } from 'react-chartjs'
 import { updateDashboardModalStatus } from '../../actions'
+import robotocatImage from '../../utilities/octodex/robotocat.png'
 
 import './index.scss'
 
 class Dashboard extends Component {
   renderDashboardSection () {
+    const { gistTags } = this.props
+
+    const langTags = Object.keys(gistTags)
+      .filter(key => key.startsWith('lang@') && key !== 'lang@All')
+
+    langTags.sort((t1, t2) => gistTags[t2].length - gistTags[t1].length)
+    console.log(JSON.stringify(langTags))
+
+    const maxNum = 5
+    const rawLabels = langTags.slice(0, langTags.length > maxNum ? maxNum : langTags.length)
+    const data = rawLabels.map(lang => gistTags[lang].length)
+    const labels = rawLabels.map(rawLabel => rawLabel.substr('@lang'.length))
+
+    if (data.length > 2) return this.buildRadarChart(data, labels)
+
+    return (
+      <div className='dashboard-section'>
+        <Image className='octocat' src={ robotocatImage } rounded/>
+        <div className='greeting'>
+          Not enough data. Try creating gists of more than two languages. Happy Coding!
+        </div>
+      </div>
+    )
+  }
+
+  buildRadarChart (data, labels) {
+    const GREY = '#C2C4D1'
     const chartData = {
-      labels: ["C++", "Java", "JavaScript", "Ruby", "Python", "Shell", "Others"],
+      labels: labels,
       datasets: [
         {
-          label: "My Language Radar",
-          fillColor: "rgba(151,187,205,0.2)",
-          strokeColor: "rgba(151,187,205,1)",
-          pointColor: "rgba(151,187,205,1)",
-          pointStrokeColor: "#fff",
-          pointHighlightFill: "#fff",
-          pointHighlightStroke: "rgba(151,187,205,1)",
-          data: [65, 59, 90, 81, 56, 55, 40]
+          label: 'My Language Stats',
+          fillColor: 'rgba(81,192,191,0.2)',
+          strokeColor: 'rgba(81,192,191,1)',
+          pointColor: 'rgba(81,192,191,1)',
+          pointStrokeColor: GREY,
+          pointHighlightFill: GREY,
+          pointHighlightStroke: 'rgba(81,192,191,1)',
+          data: data
         }
       ]
     }
@@ -30,7 +58,13 @@ class Dashboard extends Component {
 
     return (
       <div className='dashboard-section'>
-        <Radar data={ chartData } options={ chartOptions } width="350" height="250"/>
+        <Radar data={ chartData } options={ chartOptions } width="350" height="300"/>
+        <div className='greeting'>
+          { labels[0] === 'Other'
+            ? 'Hmm... Looks like you are learning a mysterious language(Other)...'
+            : `Well done! You are on the road to ${labels[0]} Master!`
+          }
+        </div>
       </div>
     )
   }
@@ -68,7 +102,8 @@ class Dashboard extends Component {
 
 function mapStateToProps (state) {
   return {
-    dashboardModalStatus: state.dashboardModalStatus
+    dashboardModalStatus: state.dashboardModalStatus,
+    gistTags: state.gistTags
   }
 }
 
