f6657ea93274c8d01f2e9784c2108b394f7074b5
Anthony Attard <anthony@anthonyattard.com>
2019-08-26 19:41:43 -0700

    add new contributor

--- app/containers/aboutPage/index.js
+++ app/containers/aboutPage/index.js
@@ -69,6 +69,7 @@ class AboutPage extends Component {
           <div className='contributor'><a href='https://github.com/rawrmonstar'>rawrmonstar</a></div>
           <div className='contributor'><a href='https://github.com/baybatu'>baybatu</a></div>
           <div className='contributor'><a href='https://github.com/samuelmeuli'>samuelmeuli</a></div>
+          <div className='contributor'><a href='https://github.com/anthonyattard'>anthonyattard</a></div>
         </div>
         <div className='setting-title'>Acknowledgement</div>
         <div className='license-section'>
