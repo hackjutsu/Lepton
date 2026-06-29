3f40d8bf4c072429e2fca18f7aaa2427eaefd122
hackjutsu <cosmo.lepton@gmail.com>
2026-06-29 16:06:05 -0700

    Render safe Markdown HTML

--- app/utilities/markdown/index.js
+++ app/utilities/markdown/index.js
@@ -3,9 +3,11 @@ import MarkdownIt from 'markdown-it'
 import { full as MdEmoji } from 'markdown-it-emoji'
 import MdTaskList from 'markdown-it-task-lists'
 import MdKatex from 'markdown-it-katex'
+import sanitizeHtml, { sanitizeInlineHtml } from './sanitizeHtml'
 
 // Configure markdown-it
 const Md = MarkdownIt({
+  html: true,
   linkify: true,
   highlight: (str, lang) => {
     if (lang && HighlightJS.getLanguage(lang)) {
@@ -19,5 +21,8 @@ const Md = MarkdownIt({
   .use(MdEmoji)
   .use(MdTaskList)
   .use(MdKatex, { throwOnError: false, errorColor: ' #cc0000' })
+
+Md.renderer.rules.html_block = (tokens, idx) => sanitizeHtml(tokens[idx].content)
+Md.renderer.rules.html_inline = (tokens, idx) => sanitizeInlineHtml(tokens[idx].content)
 
 export default Md
