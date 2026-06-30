ced46d020eca375333fee9d7dacf60fd9ab7bc2f
hackjutsu <cosmo.lepton@gmail.com>
2026-06-30 16:56:50 -0700

    Upgrade Chart.js dashboard chart

---
+++ app/containers/dashboard/chartConfig.js
@@ -0,0 +1,42 @@
+const GREY = '#C2C4D1'
+const ACCENT = 'rgba(81,192,191,1)'
+const ACCENT_FILL = 'rgba(81,192,191,0.2)'
+
+export function buildRadarChartConfig (data, labels, datasetLabel) {
+  return {
+    type: 'radar',
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: datasetLabel,
+          backgroundColor: ACCENT_FILL,
+          borderColor: ACCENT,
+          pointBackgroundColor: ACCENT,
+          pointBorderColor: GREY,
+          pointHoverBackgroundColor: GREY,
+          pointHoverBorderColor: ACCENT,
+          data: data
+        }
+      ]
+    },
+    options: {
+      responsive: false,
+      maintainAspectRatio: false,
+      plugins: {
+        legend: {
+          display: false
+        }
+      },
+      scales: {
+        r: {
+          pointLabels: {
+            font: {
+              size: 12
+            }
+          }
+        }
+      }
+    }
+  }
+}
